297773d81e2e6206e75d425601effdfb4fc4f0e0
Max Kasperowski <maxkasperowski@gmail.com>
2017-06-17 17:01:57 +0800

    upload and save files to ./uploads, must change later to buffer in memory and then save according to participant number and photonumber

--- routes/uploadPhoto.js
+++ routes/uploadPhoto.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var uploader = require('../uploader');
+var multer = require('multer');
 
 var router = express.Router();
+var upload = multer({dest: './uploads/'});
 
 /* POST upload photo. */
-router.post('/', function(req, res, next){
+router.post('/', upload.single('file'), function(req, res, next){
   uploader.uploadPhoto(req, res, function(err, res){
     if (err){
       res.render('error', {error: err});
--- uploader.js
+++ uploader.js
@@ -1,5 +1,6 @@
 
 exports.uploadPhoto = function(req, res, cb){
-  var err = new Error('There was an error uploading the file')
-  cb(err, res);
+  //var err = new Error('There was an error uploading the file')
+  console.log(req.file);
+  cb(null, res);
 }
