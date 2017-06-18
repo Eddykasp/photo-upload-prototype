306f276466ac290c1a01e9ed3ce2759466cda79f
Max Kasperowski <maxkasperowski@gmail.com>
2017-06-18 09:41:09 +0800

    change to https server

--- uploader.js
+++ uploader.js
@@ -1,6 +1,8 @@
 
 exports.uploadPhoto = function(req, res, cb){
   //var err = new Error('There was an error uploading the file')
+  var userId = req.body.userId;
+  var password = req.body.password;
   console.log(req.file);
   cb(null, res);
 }
